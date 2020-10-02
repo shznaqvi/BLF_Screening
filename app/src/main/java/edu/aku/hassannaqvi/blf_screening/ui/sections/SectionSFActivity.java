1a84a0a096764af6abfea070a06a1baf93b4ba44
Imran Daar <daarimran16@gmail.com>
2020-10-02 16:37:51 +0500

    add Layout and save draft validation.

---
+++ app/src/main/java/edu/aku/hassannaqvi/blf_screening/ui/sections/SectionSFActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.blf_screening.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.blf_screening.R;
+
+public class SectionSFActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_sf);
+    }
+}
