6022c33d97a1e1e871ff2a5eb669ef35bb81e4f3
SAJID LATIF <indus1530@gmail.com>
2020-10-28 15:29:39 +0500

    SectionSFActivity question_SF6a minDate to -3 days

--- app/src/main/java/edu/aku/hassannaqvi/blf_screening/ui/sections/SectionSFActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/blf_screening/ui/sections/SectionSFActivity.java
@@ -120,6 +120,7 @@ public class SectionSFActivity extends AppCompatActivity {
             }
         });
 
+
         bi.sf701.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -256,7 +257,7 @@ public class SectionSFActivity extends AppCompatActivity {
         });
 
 
-        bi.sf2.addTextChangedListener(new TextWatcher() {
+        /*bi.sf2.addTextChangedListener(new TextWatcher() {
 
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -281,7 +282,7 @@ public class SectionSFActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
 
             }
-        });
+        });*/
 
     }
 
@@ -289,6 +290,7 @@ public class SectionSFActivity extends AppCompatActivity {
         bi.sf1701.setChecked(sf17Flag);
         bi.sf1702.setChecked(!sf17Flag);
     }
+
 
     public void BtnContinue() {
         bi.pBar3.setVisibility(View.GONE);
@@ -312,6 +314,7 @@ public class SectionSFActivity extends AppCompatActivity {
             Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
         }
     }
+
 
     private boolean RetrieveSrcID() {
         final OneTimeWorkRequest workRequest1 = new OneTimeWorkRequest.Builder(DataUpWorkerSF.class).build();
@@ -426,6 +429,7 @@ public class SectionSFActivity extends AppCompatActivity {
         return false;
     }
 
+
     private boolean UpdateDB() {
 
         DatabaseHelper db = MainApp.appInfo.getDbHelper();
@@ -440,6 +444,7 @@ public class SectionSFActivity extends AppCompatActivity {
             return false;
         }
     }
+
 
     private void SaveDraft() throws JSONException {
 
@@ -596,148 +601,19 @@ public class SectionSFActivity extends AppCompatActivity {
         //MainApp.formsSF.setSf1902(bi.setSf1902.getText().toString());
 
         MainApp.formsSF.setSf20(bi.sf20.getText().toString());
-
-       /* JSONObject json = new JSONObject();
-
-        json.put("sf101", bi.sf101.getText().toString());
-        json.put("sf102", bi.sf102.getText().toString());
-        json.put("sf103", bi.sf103.getText().toString());
-        json.put("sf104", bi.sf104.getText().toString());
-        json.put("sf105", bi.sf105.getText().toString());
-
-        json.put("sf2", bi.sf2.getText().toString());
-        json.put("sf3", bi.sf3.getText().toString());
-        json.put("sf4", bi.sf4.getText().toString());
-        json.put("sf5", bi.sf5.getText().toString());
-        json.put("sf6", bi.sf6.getText().toString());
-
-        json.put("sf701", bi.sf701.getText().toString());
-        json.put("sf702", bi.sf702.getText().toString());
-
-        json.put("sf8", bi.sf8.getText().toString());
-
-        json.put("sf9", bi.sf901.isChecked() ? "1"
-                : bi.sf902.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf10", bi.sf10.getText().toString());
-
-        json.put("sf11", bi.sf1101.isChecked() ? "1"
-                : bi.sf1102.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf12", bi.sf1201.isChecked() ? "1"
-                : bi.sf1202.isChecked() ? "2"
-                : bi.sf1203.isChecked() ? "3"
-                : bi.sf1204.isChecked() ? "4"
-                : bi.sf1296.isChecked() ? "96"
-                : "-1");
-        json.put("sf1296x", bi.sf1296x.getText().toString());
-
-        json.put("sf1301", bi.sf130101.isChecked() ? "1"
-                : bi.sf130102.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1302", bi.sf130201.isChecked() ? "1"
-                : bi.sf130202.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1303", bi.sf130301.isChecked() ? "1"
-                : bi.sf130302.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1304", bi.sf130401.isChecked() ? "1"
-                : bi.sf130402.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1305", bi.sf130501.isChecked() ? "1"
-                : bi.sf130502.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1306", bi.sf130601.isChecked() ? "1"
-                : bi.sf130602.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1307", bi.sf130701.isChecked() ? "1"
-                : bi.sf130702.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1308", bi.sf130801.isChecked() ? "1"
-                : bi.sf130802.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1309", bi.sf130901.isChecked() ? "1"
-                : bi.sf130902.isChecked() ? "2"
-                : bi.sf1396.isChecked() ? "96"
-                : "-1");
-        json.put("sf1396x", bi.sf1396x.getText().toString());
-
-        json.put("sf14", bi.sf1401.isChecked() ? "1"
-                : bi.sf1402.isChecked() ? "2"
-                : bi.sf1403.isChecked() ? ""
-                : "-1");
-        json.put("sf1403x", bi.sf1403x.getText().toString());
-
-        json.put("sf1501", bi.sf150101.isChecked() ? "1"
-                : bi.sf150102.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1502", bi.sf150201.isChecked() ? "1"
-                : bi.sf150202.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1503", bi.sf150301.isChecked() ? "1"
-                : bi.sf150302.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1504", bi.sf150401.isChecked() ? "1"
-                : bi.sf150402.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1505", bi.sf150501.isChecked() ? "1"
-                : bi.sf150502.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1506", bi.sf150601.isChecked() ? "1"
-                : bi.sf150602.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1507", bi.sf150701.isChecked() ? "1"
-                : bi.sf150702.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1508", bi.sf150801.isChecked() ? "1"
-                : bi.sf150802.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf1509", bi.sf150901.isChecked() ? "1"
-                : bi.sf150902.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf16", bi.sf1601.isChecked() ? "1"
-                : bi.sf1602.isChecked() ? "2"
-                : bi.sf1698.isChecked() ? "98"
-                : "-1");
-
-        json.put("sf17", bi.sf1701.isChecked() ? "1"
-                : bi.sf1702.isChecked() ? "2"
-                : "-1");
-
-        json.put("sf18", bi.sf1801.isChecked() ? "1"
-                : bi.sf1802.isChecked() ? "2"
-                : "-1");
-
-        *//*json.put("sf19", bi.sf19.getText().toString());*//*
-
-        json.put("sf1901", bi.sf1901.getText().toString());
-        json.put("sf1902", bi.sf1902.getText().toString());
-        json.put("sf20", bi.sf20.getText().toString());*/
+        MainApp.setGPS(this);
 
     }
+
 
     private boolean formValidation() {
-        return Validator.emptyCheckingContainer(this, bi.GrpName);
+
+        if (Validator.emptyCheckingContainer(this, bi.GrpName)) return false;
+
+        return !bi.sf2.getText().toString().equals(bi.sf5.getText().toString());
+
     }
+
 
     public void BtnEnd() {
         oF = new Intent(this, MainActivity.class);
