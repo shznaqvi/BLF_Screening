3d10a517ba5157d8b215a12196598dff5fdcf7ef
muhammad.fazal <fazal.abbas4@gmail.com>
2020-11-20 16:50:26 +0500

    Add weekly follow up module A section 1 with layout and activity

--- app/src/main/java/edu/aku/hassannaqvi/blf_screening/ui/sections/SetionWFA01Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/blf_screening/ui/sections/SetionWFA01Activity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SetionWFDActivity extends AppCompatActivity {
+import edu.aku.hassannaqvi.blf_screening.R;
+
+public class SetionWFA01Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setion_w_f_d);
+        setContentView(R.layout.activity_setion_w_f_a01);
     }
 }
